e6ec8006638adb4dd0f14b611a7f3fb08315591a
Mike Garlick <mike@earthlingsnw.com>
2018-03-22 19:45:53 -0700

    Update to move to MySql for storage, and get things running again under local Ubuntu

--- db/migrate/20180317022924_create_graphics.rb
+++ db/migrate/20180317022924_create_graphics.rb
@@ -11,16 +11,14 @@ class CreateGraphics < ActiveRecord::Migration[5.1]
       t.timestamps
     end
     change_table :coins do |t|
-      t.integer :icon_id, null:true
+      t.references :icon, references: :graphics, null:true
     end
     change_table :editions do |t|
-      t.integer :icon_id, null:true
+      t.references :icon, references: :graphics, null:true
     end
 
     add_foreign_key :coins, :graphics, column: :icon_id, on_delete: :nullify
-    add_index :coins, :icon_id, name: "index_coins_on_icon_id"
 
     add_foreign_key :editions, :graphics, column: :icon_id, on_delete: :nullify
-    add_index :editions, :icon_id, name: "index_editions_on_icon_id"
   end
 end
--- test/controllers/coins_controller_test.rb
+++ test/controllers/coins_controller_test.rb
@@ -3,15 +3,19 @@ require 'test_helper'
 class CoinsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @coin = coins(:one)
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get index" do
+    puts coins_path
     get coins_path
     assert_response :success
     assert_select "title", "All Coins | Coin Catalog"
   end
 
   test "should get item" do
+    log_in_as(@user)
     get edit_coin_path(@coin)
     assert_response :success
   end
